e370b4486d15b605e5ff502945935b4ed762c829
Moritz Dittmann <moe@posteo.de>
2013-04-05 16:58:42 +0200

    Initial Commit

---
+++ FoodStock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoodStock
+//
+//  Created by Moritz on 05.04.13.
+//  Copyright (c) 2013 dittmann.moritz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
